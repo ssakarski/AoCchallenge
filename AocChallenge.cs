73f344e390e90dc7c333283dbf507a4f5539e505
ssakarski <ssakarski96@gmail.com>
2022-01-08 16:23:25 +0200

    Separated the methods into the AocChallenge class.

---
+++ AocChallenge.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGit
+{
+    public static class AocChallenge
+    {
+        public static void binaryDiagnostic(string[] bin)
+        {
+            string currentBitLine = "";
+            int[] currentBitCount = new int[bin[0].Length];
+            string gammaRate = "";
+            string epsilonRate = "";
+            // Finding the most common bits
+            for (int i = 0; i < bin.Length; i++)
+            {
+                currentBitLine = bin[i];
+                for (int j = 0; j < bin[i].Length; j++)
+                {
+                    if (currentBitLine[j] == '1')
+                    {
+                        currentBitCount[j] += 1;
+                    }
+                }
+            }
+            // Finding the gamma and epsilon rates
+            for (int i = 0; i < currentBitCount.Length; i++)
+            {
+                if (currentBitCount[i] < bin.Length / 2)
+                {
+                    gammaRate += "0";
+                    epsilonRate += "1";
+                }
+                else
+                {
+                    gammaRate += "1";
+                    epsilonRate += "0";
+                }
+            }
+            int decimalGammaRate = Convert.ToInt32(gammaRate, 2);
+            int decimalEpsilonRate = Convert.ToInt32(epsilonRate, 2);
+            Console.WriteLine(decimalGammaRate * decimalEpsilonRate);
+        }
+        public static void submarineMovement(string[] mov)
+        {
+            int forward = 0;
+            int depth = 0;
+            int aim = 0;
+            string[] test = new string[2];
+            for (int i = 0; i < mov.Length; i++)
+            {
+                test = mov[i].Split(" ");
+                switch (test[0])
+                {
+                    case "forward":
+                        {
+                            forward += int.Parse(test[1]);
+                            depth = depth + (aim * int.Parse(test[1]));
+                            break;
+                        }
+                    case "up":
+                        {
+                            aim -= int.Parse(test[1]);
+                            break;
+                        }
+                    default:
+                        {
+                            aim += int.Parse(test[1]);
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine("forward: " + forward);
+            Console.WriteLine("depth: " + depth);
+            Console.WriteLine("aim: " + aim);
+            Console.WriteLine("distance: {0}", depth * forward);
+        }
+        public static void depthCount(string[] depth)
+        {
+            /*  Test input
+                199
+                200
+                208
+                210
+                200
+                207
+                240
+                269
+                260
+                263
+                Test output
+                7
+            */
+            int[] d = Array.ConvertAll(depth, item => int.Parse(item));
+            int[] depthHolder = new int[d.Length - 2];
+            int firstSum = 0;
+            int counter = 0;
+            for (int i = 0; i < d.Length - 2; i++)
+            {
+                firstSum = d[i] + d[i + 1] + d[i + 2];
+                depthHolder[i] = firstSum;
+            }
+            for (int i = 0; i < depthHolder.Length - 1; i++)
+            {
+                if (depthHolder[i] < depthHolder[i + 1])
+                {
+                    counter += 1;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.IO;
 using System.Collections.Generic;
+using System.Net.Http;
 
 namespace TestingGit
 {
@@ -10,120 +11,16 @@ namespace TestingGit
         static void Main(string[] args)
         {
             // Day 1 of Advent of Code !!!
-            // string[] depth = File.ReadAllLines(@"C:\Users\User\source\repos\TestingGit\TestInputs\depthInput.txt");
-            // depthCount(depth);
+            // string[] depth = File.ReadAllLines(@"..\..\..\TestInputs\depthInput.txt");
+            // AocChallenge.depthCount(depth);
 
             // Day 2 of Advent of Code !!!
-            // string[] movement = File.ReadAllLines(@"C:\Users\User\source\repos\TestingGit\TestInputs\submarineMovement.txt");
-            // submarineMovement(movement);
-            // Day 3 of Advent of Code !!!  D:\Users\User\Desktop\testInputs.txt    C:\Users\User\source\repos\TestingGit\TestInputs\binDiagnostic.txt
-            string[] binDiagnostic = File.ReadAllLines(@"C:\Users\User\source\repos\TestingGit\TestInputs\binDiagnostic.txt");
-            binaryDiagnostic(binDiagnostic);
-        }
+            // string[] movement = File.ReadAllLines(@"..\..\..\TestInputs\submarineMovement.txt");
+            // AocChallenge.submarineMovement(movement);
 
-        static void binaryDiagnostic(string[] bin)
-        {
-            string currentBitLine = "";
-            int[] currentBitCount = new int[bin[0].Length];
-            string gammaRate = "";
-            string epsilonRate = "";
-            // Finding the most common bits
-            for (int i = 0; i < bin.Length; i++)
-            {
-                currentBitLine = bin[i];
-                for (int j = 0; j < bin[i].Length; j++)
-                {
-                    if (currentBitLine[j] == '1')
-                    {
-                        currentBitCount[j] += 1;
-                    }
-                }
-            }
-            // Finding the gamma and epsilon rates
-            for (int i = 0; i < currentBitCount.Length; i++)
-            {
-                if (currentBitCount[i] < bin.Length/2)
-                {
-                    gammaRate += "0";
-                    epsilonRate += "1";
-                }
-                else
-                {
-                    gammaRate += "1";
-                    epsilonRate += "0";
-                }
-            }
-            int decimalGammaRate = Convert.ToInt32(gammaRate, 2);
-            int decimalEpsilonRate = Convert.ToInt32(epsilonRate, 2);
-            Console.WriteLine(decimalGammaRate*decimalEpsilonRate);
-        }
-        static void submarineMovement(string[] mov)
-        {
-            int forward = 0;
-            int depth = 0;
-            int aim = 0;
-            string[] test = new string[2];
-            for (int i = 0; i < mov.Length; i++)
-            {
-                test = mov[i].Split(" ");
-                switch (test[0])
-                {
-                    case "forward": 
-                        { 
-                            forward += int.Parse(test[1]);
-                            depth = depth+(aim*int.Parse(test[1]));
-                            break;
-                        }
-                    case "up": 
-                        {
-                            aim -= int.Parse(test[1]);
-                            break;
-                        }
-                    default:
-                        {
-                            aim += int.Parse(test[1]);
-                            break;
-                        }
-                }
-            }
-            Console.WriteLine("forward: "+forward);
-            Console.WriteLine("depth: " + depth);
-            Console.WriteLine("aim: " + aim);
-            Console.WriteLine("distance: {0}",depth * forward);
-        }
-        static void depthCount(string[] depth)
-        {
-            /*  Test input
-                199
-                200
-                208
-                210
-                200
-                207
-                240
-                269
-                260
-                263
-                Test output
-                7
-            */
-            int[] d = Array.ConvertAll(depth, item => int.Parse(item));
-            int[] depthHolder = new int[d.Length - 2];
-            int firstSum = 0;
-            int counter = 0;
-            for (int i = 0; i < d.Length - 2; i++)
-            {
-                firstSum = d[i] + d[i + 1] + d[i + 2];
-                depthHolder[i] = firstSum;
-            }
-            for (int i = 0; i < depthHolder.Length - 1; i++)
-            {
-                if (depthHolder[i] < depthHolder[i + 1])
-                {
-                    counter += 1;
-                }
-            }
-            Console.WriteLine(counter);
+            // Day 3 of Advent of Code !!!  D:\Users\User\Desktop\testInputs.txt    @"..\..\..\TestInputs\binDiagnostic.txt"
+               string[] binDiagnostic = File.ReadAllLines(@"..\..\..\TestInputs\binDiagnostic.txt");
+               AocChallenge.binaryDiagnostic(binDiagnostic);
         }
     }
 }
